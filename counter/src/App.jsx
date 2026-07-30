3cf7a4daf977581a39186f2a8f3bbccecea1a206
Priyank Shukla <umeshusha1974@gmail.com>
2026-07-30 23:19:50 +0530

    feat: add functionality to increase and decrease count by 5

--- counter/src/App.jsx
+++ counter/src/App.jsx
@@ -6,32 +6,53 @@ function App() {
   function increase() {
     setCount(count + 1);
   }
-  function increase() {
+
+  function increaseby5() {
     setCount(count + 5);
   }
-  function increase() {
-    setCount(count - 5);
+  
+  function decrease(){
+    if(count > 0){
+    setCount(count - 1)
+    }
   }
 
-  function decrease(){
-    setCount(count - 1)
+function decreaseby5() {
+  if(count > 5){
+    setCount(count - 5);
   }
+}
 
   function reset(){
     setCount(0)
   }
 
-
+  
+  
   return (
     <>
       <h1>Count: {count}</h1>
 
+      {count >= 10 && (
+    <p>Tu toh double digit mai pahuch gya lala 🎉</p>
+       )}
+
       <button onClick={increase}>
         badha de bhai
       </button>
+
+      <button onClick={increaseby5}>
+         bhai 5 se badha na
+      </button>
+
       <button onClick={decrease}>
         kum kare bhai
       </button>
+
+      <button onClick={decreaseby5}>
+        bhai 5 se kam kar na
+      </button>
+
       <button onClick={reset}>
          reset kar de
       </button>
