24562907b282ec727158fd66783e621c70a02555
Priyank Shukla <umeshusha1974@gmail.com>
2026-07-30 21:12:40 +0530

    feat: implement counter functionality with increase, decrease, and reset features

--- counter/src/App.jsx
+++ counter/src/App.jsx
@@ -1,7 +1,42 @@
-function App(){
-  return(
-    <h1>Hello React</h1>
-  )
+import { useState } from "react";
+
+function App() {
+  const [count, setCount] = useState(0);
+
+  function increase() {
+    setCount(count + 1);
+  }
+  function increase() {
+    setCount(count + 5);
+  }
+  function increase() {
+    setCount(count - 5);
+  }
+
+  function decrease(){
+    setCount(count - 1)
+  }
+
+  function reset(){
+    setCount(0)
+  }
+
+
+  return (
+    <>
+      <h1>Count: {count}</h1>
+
+      <button onClick={increase}>
+        badha de bhai
+      </button>
+      <button onClick={decrease}>
+        kum kare bhai
+      </button>
+      <button onClick={reset}>
+         reset kar de
+      </button>
+    </>
+  );
 }
 
 export default App;
