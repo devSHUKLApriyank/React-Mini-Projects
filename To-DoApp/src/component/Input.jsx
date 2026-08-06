aea06bc6c0c5c5af32338e3d35519d77ae31fd5a
Priyank Shukla <umeshusha1974@gmail.com>
2026-08-06 17:14:52 +0530

    feat: update Todo component to manage todos and input state, and add functionality to add new tasks

--- To-DoApp/src/component/Input.jsx
+++ To-DoApp/src/component/Input.jsx
@@ -1,10 +1,11 @@
 import React from 'react'
 
-const Input = ({input,setInput}) => {
+const Input = ({input,setInput,addTodo}) => {
     
   return (
     <div>
-      
+      <input type="text" placeholder='Add a task' value={input} onChange={(e)=>setInput(e.target.value)} />
+      <button onClick={addTodo}>Add</button>
     </div>
   )
 }
--- To-DoApp/src/component/Todo.jsx
+++ To-DoApp/src/component/Todo.jsx
@@ -2,8 +2,29 @@ import React from 'react'
 import { useState } from 'react'
 
 const Todo = () => {
-    const[todos , setTodos] = useState("")
-    const[input , setInput] = useState([])
+    const[todos , setTodos] = useState([])
+    const[input , setInput] = useState("")
+
+    function addtodo(){
+      if(input.trim() === ""){
+        return
+      }
+
+      const newTodo = {
+        id: Date.now(),
+        text:input,
+        completed:false,
+      };
+
+      setTodos([
+        ...todos,
+        newTodo
+      ])
+
+      setInput("")
+
+    }
+
   return (
     <div>
       <h1>To-Do App</h1>
