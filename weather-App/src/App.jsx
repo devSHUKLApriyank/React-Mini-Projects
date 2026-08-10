343ac0f4542327e9ac9cd6938f2ee2a3fca7e080
Priyank Shukla <umeshusha1974@gmail.com>
2026-08-10 11:29:26 +0530

    Add SVG assets for React and Vite logos

--- weather-App/src/App.jsx
+++ weather-App/src/App.jsx
@@ -1,9 +1,23 @@
 import React from 'react'
 
-const App = () => {
+function App() {
   return (
-    <div>
-      
+    <div className='min-h-screen flex items-center justify-center'>
+      <div className="w-full max-w-md bg-white p-6 rounded-2xl shadow-lg">
+        <h1 className='text-3xl font-bold mb-6 text-center'>
+          Weather App
+        </h1>
+        <div className='flex gap-2'>
+          <input
+          type='text'
+          placeholder='Enter city name'
+          className='flex-1 border rounded-lg px-4 py-2 outline-none' 
+          />
+          <button className='bg-blue-500 text-white px-4 py-2 rounded-lg'>
+            Search
+          </button>
+        </div>
+      </div>
     </div>
   )
 }
